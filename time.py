458134e906b02f735e20f710df156429b8aa1935
ColdBacon <bogumila.walkowiak@op.pl>
2019-03-17 11:08:05 +0100

    new list generator

--- time.py
+++ time.py
@@ -18,16 +18,18 @@ for i in range(50):                 #with repetition
     B.append(random.randint(1,10))
 
 C=[]
-for i in range(100):                #increasing
-    C.append(i)
-
+for i in range(50):                 #increasing
+    C.append(random.randint(1,10))
+C.sort()
 
 D=[]
-for i in range(100,-1,-1):          #decreasing
-    D.append(i)
+for i in range(50):                 #decreasing
+    D.append(random.randint(1,10))
+D.sort()
+D=D[::-1]
     
 E=[]
-for i in range(9):                  #"v" shaped
+"""for i in range(9):               #"v" shaped
     E.append(0)
 
 index1=0
@@ -39,6 +41,20 @@ for i in range(10,0,-1):
     else:
         E[index2*(-1)]=i
         index2=index2+1
+print (E)"""
+E1=[]
+E2=[]
+for i in range(10):
+    E.append(random.randint(1,20))
+E.sort()
+
+for i in range(10):
+    if i%2==0:
+        E1.append(E[i])
+    else:
+        E2.append(E[i])
+E1=E1[::-1]
+E=E1+E2
 print (E)
 
 start = datetime.datetime.now()
